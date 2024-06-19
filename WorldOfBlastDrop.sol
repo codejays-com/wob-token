630384b9f24f41b8cb7f3bc8ffca9c000cd58406
Lucas Lopes <lucass.code@gmail.com>
2024-06-19 12:42:28 -0400

    Update WorldOfBlastDrop.sol

--- WorldOfBlastDrop.sol
+++ WorldOfBlastDrop.sol
@@ -10,11 +10,9 @@ interface IERC721Enumerable is IERC721 {
 }
 
 contract WorldOfBlastDrop is Ownable {
-    address private contractTokenAddress;
-    address private contractNFTAddress;
-
-    uint256 constant PERCENT_MULTIPLIER = 1000000000000000000;
-    uint256 public defaultTokenEarnsPercent = 338100000000; // 0.00003381 percent
+    address public contractTokenAddress;
+    address public contractNFTAddress;
+    uint256 public defaultTokenEarnsPercent = 3381000000000; // 0.00003381 percent
     uint256 public targetAveragePercent = 98;
 
     mapping(address => bool) public authorizedToUseContract;
@@ -79,6 +77,18 @@ contract WorldOfBlastDrop is Ownable {
         return deliveryEarns;
     }
 
+    function handleNFTEarnings(address to) external onlyAuthorizedContract {
+        IERC721Enumerable currentToken = IERC721Enumerable(contractNFTAddress);
+        uint256 randomNumber = uint256(keccak256(abi.encodePacked(block.timestamp)));
+        uint256 randomInRange = randomNumber % 100;
+        uint256 balance = currentToken.balanceOf(address(this));
+        if (balance > 0 && randomInRange == 0) {
+            uint256 randomIndex = uint256(keccak256(abi.encodePacked(block.timestamp, to))) % balance;
+            uint256 tokenId = currentToken.tokenOfOwnerByIndex(address(this), randomIndex);
+            currentToken.safeTransferFrom(address(this), to, tokenId);
+        }
+    }
+
     function transferFromERC20(uint256 amount, address to) external onlyAuthorizedContract { 
         IERC20 currentToken = IERC20(contractTokenAddress);
         uint256 currentAmount = currentToken.balanceOf(address(this));
@@ -88,13 +98,8 @@ contract WorldOfBlastDrop is Ownable {
         currentToken.transfer(to, amount);
     }
 
-    function transferFromERC721(address to) external onlyAuthorizedContract {
+    function transferFromERC721(address to, uint256 tokenId) external onlyAuthorizedContract {
         IERC721Enumerable currentToken = IERC721Enumerable(contractNFTAddress);
-        uint256 balance = currentToken.balanceOf(address(this));
-        if (balance > 0) {
-            uint256 randomIndex = uint256(keccak256(abi.encodePacked(block.timestamp, to))) % (balance + 1);
-            uint256 tokenId = currentToken.tokenOfOwnerByIndex(address(this), randomIndex);
-            currentToken.safeTransferFrom(address(this), to, tokenId);
-        }
+        currentToken.safeTransferFrom(address(this), to, tokenId);
     }
 }
