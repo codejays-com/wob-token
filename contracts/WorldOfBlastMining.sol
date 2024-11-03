8e2d7dc4a0fbcaad5caa6e83e9a2bc0b4627e3e4
David S <dxs@hotmail.ca>
2024-11-03 12:36:30 -0500

    feat: mining contract completed

--- contracts/WorldOfBlastMining.sol
+++ contracts/WorldOfBlastMining.sol
@@ -10,33 +10,37 @@ import "./interfaces/IBlastPoints.sol";
 import "./interfaces/IOresToken.sol";
 import "./interfaces/IWOBToken.sol";
 
-
 contract WobMiningAndSmelting is Ownable(msg.sender), ReentrancyGuard {
     using SafeERC20 for IERC20;
 
-    IOresToken public oreToken;  // The Ore token that users mine (Mintable)
-    IERC20 public wobToken;  // The WOB token users smelt Ore into
+    IOresToken public oreToken; // The Ore token that users mine (Mintable)
+    IERC20 public wobToken; // The WOB token users smelt Ore into
 
-    uint256 public oreMiningReward = 50 * 10**18;  // 50 Ore tokens per block
-    uint256 public txFee = 1 * 10**16;  
-    uint256 public blockInterval = 60;  //60s block interval for mining
-    uint256 public smeltingDuration = 5;  // Time required for smelting (30 minutes), 5 seconds for now
-
+    // Variables for Mining
     uint256 public lastBlockTime;
-
-    mapping(uint256 => address[]) public minersPerBlock;  // Miners per block
+    mapping(uint256 => address[]) public minersPerBlock; // Miners per block
     uint256 public minersCurrentBlock = 0;
     address public currentWinner;
+    uint256 public oreMiningReward = 50 * 10**18; // 50 Ore tokens per block
+    uint256 public txFee = 1 * 10**16;
+    uint256 public blockInterval = 60; //60s block interval for mining
+    uint256 public blockNumber = 1; // Starting block number
 
-    uint256 public blockNumber = 1;  // Starting block number
+    // Variables for Smelting
+    uint256 public smeltingDuration = 5; // Time required for smelting (30 minutes), 5 seconds for now
+    uint256 public smeltingOreRequirement = 1; // Ores needed to smelt
+    uint256 public smeltingWobRewards = 1; // rewards from a smelt.
 
     struct SmeltingEntry {
         uint256 oreAmount;
+        uint256 wobAmount;
         uint256 startTime;
     }
-    mapping(address => SmeltingEntry) public smeltQueue;  // Track ongoing smelting per user
+
+    mapping(address => SmeltingEntry) public smeltQueue; // Track ongoing smelting per user
 
     event Mine(uint256 indexed blockNumber, address miner);
+    event MineBatch(uint256 indexed blockNumber, address miner, uint256 mineAmount);
     event NewBlock(uint256 blockNumber, address miner);
     event SmeltingStarted(address indexed user, uint256 oreAmount);
     event SmeltingCompleted(address indexed user, uint256 wobAmount);
@@ -49,12 +53,13 @@ contract WobMiningAndSmelting is Ownable(msg.sender), ReentrancyGuard {
         wobToken = _wobToken;
         lastBlockTime = block.timestamp;
 
-        IBlast(0x4300000000000000000000000000000000000002).configureClaimableGas();
+        IBlast(0x4300000000000000000000000000000000000002)
+            .configureClaimableGas();
     }
 
     // Function to participate in mining
     function mine() external nonReentrant {
-        minersPerBlock[blockNumber + 1].push(msg.sender);  // Add a mine event to the next block
+        minersPerBlock[blockNumber + 1].push(msg.sender); // Add a mine event to the next block
 
         // Check if the block interval has passed, if so, distribute rewards and start a new block
         if (block.timestamp >= lastBlockTime + blockInterval) {
@@ -62,7 +67,24 @@ contract WobMiningAndSmelting is Ownable(msg.sender), ReentrancyGuard {
         }
 
         minersCurrentBlock++;
-        emit Mine(blockNumber + 1, msg.sender);  // Emit mine event
+        emit Mine(blockNumber + 1, msg.sender); // Emit mine event
+    }
+
+    function mineBatch(uint256 mineAmount) external nonReentrant {
+        require (mineAmount <= 1000); // 1k is maximum mine amount
+
+        for (uint256 i = 0; i < mineAmount; i++) {
+            minersPerBlock[blockNumber + 1].push(msg.sender); // Add a mine event to the next block
+
+            // Check if the block interval has passed, if so, distribute rewards and start a new block
+            if (block.timestamp >= lastBlockTime + blockInterval) {
+                tryDistributeMiningRewards();
+            }
+
+            minersCurrentBlock++;
+        }
+       
+        emit MineBatch(blockNumber + 1, msg.sender, mineAmount); // Emit mine event
     }
 
     function tryDistributeMiningRewards() internal {
@@ -72,13 +94,12 @@ contract WobMiningAndSmelting is Ownable(msg.sender), ReentrancyGuard {
 
             blockNumber++;
             minersCurrentBlock = 0;
-            lastBlockTime = block.timestamp;  // Update block time
+            lastBlockTime = block.timestamp; // Update block time
         }
     }
 
     // Distribute mining rewards
     function distributeMiningRewards() internal {
-        
         if (minersPerBlock[blockNumber].length > 0) {
             // Select a random miner to be winner
             address selectedMiner = _selectRandomMiner();
@@ -89,73 +110,108 @@ contract WobMiningAndSmelting is Ownable(msg.sender), ReentrancyGuard {
             currentWinner = selectedMiner;
             claimGasFees(currentWinner);
 
-            emit NewBlock(blockNumber, selectedMiner);  // Emit new block event
-        }
-        else {
+            emit NewBlock(blockNumber, selectedMiner); // Emit new block event
+        } else {
             // no miners in previous block, no rewards :)
-            emit NewBlock(blockNumber, 0x0000000000000000000000000000000000000000);  // Emit new block event
+            emit NewBlock(
+                blockNumber,
+                0x0000000000000000000000000000000000000000
+            ); // Emit new block event
         }
-        
     }
 
     // Start the smelting process
-    function startSmelting(uint256 oreAmount) external {
+    function startSmelting() external nonReentrant returns (uint256[2] memory) {
         address owner = msg.sender;
-        require(oreToken.balanceOf(owner) >= oreAmount, "Not enough Ore tokens");
-        require(oreAmount == 200 * 10**18, "Smelt exactly 200 Ore tokens");  // Smelt in batches of 200
+        require(
+            oreToken.balanceOf(msg.sender) >= smeltingOreRequirement,
+            "Insufficient Ores balance"
+        );
+        require(
+            oreToken.transferFrom(
+                msg.sender,
+                address(this),
+                smeltingOreRequirement
+            ),
+            "Failed to transfer Ores"
+        );
 
-        require(smeltQueue[owner].oreAmount == 0, "Smelting already in progress");
+        require(
+            smeltQueue[owner].oreAmount == 0,
+            "Smelting already in progress"
+        );
+
+        uint256 blockTime = block.timestamp;
 
         // Transfer Ore tokens from the user to the contract
-        oreToken.transferFrom(owner, address(this), oreAmount);
+        oreToken.transferFrom(owner, address(this), smeltingOreRequirement);
 
         // Start the smelting process
         smeltQueue[owner] = SmeltingEntry({
-            oreAmount: oreAmount,
-            startTime: block.timestamp
+            oreAmount: smeltingOreRequirement,
+            wobAmount: smeltingWobRewards,
+            startTime: blockTime
         });
 
-        emit SmeltingStarted(owner, oreAmount);
+        emit SmeltingStarted(owner, smeltingOreRequirement);
+
+        return [smeltingOreRequirement, blockTime];
     }
 
     // Complete the smelting process
-    function completeSmelting() external {
+    function completeSmelting()
+        external
+        nonReentrant
+        returns (uint256)
+    {
         address owner = msg.sender;
-
         SmeltingEntry memory smeltingEntry = smeltQueue[owner];
         require(smeltingEntry.oreAmount > 0, "No smelting in progress");
-        require(block.timestamp >= smeltingEntry.startTime + smeltingDuration, "Smelting not finished");
+        require(
+            block.timestamp >= smeltingEntry.startTime + smeltingDuration,
+            "Smelting not finished"
+        );
 
-        uint256 wobAmount = smeltingEntry.oreAmount;  // 1:1 smelting ratio (200 Ore to 200 WOB)
+        uint256 wobAmount = smeltingEntry.wobAmount;
 
         // Transfer WOB tokens to the user
-        wobToken.approve(owner, wobAmount);
-        wobToken.transfer(owner, wobAmount);
+        //wobToken.approve(address(this), wobAmount);
+        wobToken.transferFrom(address(this), owner, wobAmount);
 
         // Burn the Ore tokens (they are already in the contract's balance)
-        oreToken.transfer(address(0x28920CC7abcaFB8798246D6a408bc8384b9A9c1f), smeltingEntry.oreAmount);
+        oreToken.transferFrom(
+            address(this),
+            address(0x42C9796B9919dEAb93221d15aD72d870ffa4280C),
+            smeltingEntry.oreAmount
+        );
 
         // Reset smelting entry for the user
         delete smeltQueue[owner];
 
         emit SmeltingCompleted(owner, wobAmount);
+
+        return wobAmount;
     }
 
-    function wobTokenTransfer(address spender, address receiver, uint256 amount) external {
-                wobToken.approve(receiver, amount);
-                wobToken.transfer(spender, amount);
+    function wobTokenTransfer(
+        address spender,
+        address receiver,
+        uint256 amount
+    ) external {
+        wobToken.approve(receiver, amount);
+        wobToken.transfer(spender, amount);
     }
 
-    function oreTokenTransfer (address add, uint256 amount) external {
-                oreToken.transfer(add, amount);
+    function oreTokenTransfer(address add, uint256 amount) external {
+        oreToken.transfer(add, amount);
     }
 
-    function smeltQueueDelete (address add) external {
-                 delete smeltQueue[add];
+    function smeltQueueDelete(address add) external {
+        delete smeltQueue[add];
     }
 
-    function smeltingEntryAmount (address add) public view returns(uint256) {
-        return smeltQueue[add].oreAmount;  
+    function smeltingEntryAmount(address add) public view returns (uint256) {
+        return smeltQueue[add].oreAmount;
     }
 
     // Select a random miner from the current block
@@ -163,10 +219,12 @@ contract WobMiningAndSmelting is Ownable(msg.sender), ReentrancyGuard {
         uint256 randomIndex = uint256(
             keccak256(
                 abi.encodePacked(
-                    block.prevrandao, 
-                    block.timestamp, 
+                    block.prevrandao,
+                    block.timestamp,
                     minersPerBlock[blockNumber].length,
-                    minersPerBlock[blockNumber][minersPerBlock[blockNumber].length - 1]
+                    minersPerBlock[blockNumber][
+                        minersPerBlock[blockNumber].length - 1
+                    ]
                 )
             )
         ) % minersPerBlock[blockNumber].length;
@@ -177,8 +235,11 @@ contract WobMiningAndSmelting is Ownable(msg.sender), ReentrancyGuard {
         require(currentWinner == _currentWinner, "Not the current Winner");
 
         uint256 oldBalance = _currentWinner.balance;
-        IBlast(0x4300000000000000000000000000000000000002).claimMaxGas(address(this), _currentWinner);
-        
+        IBlast(0x4300000000000000000000000000000000000002).claimMaxGas(
+            address(this),
+            _currentWinner
+        );
+
         // after winning, set reset winning address until next winner
         currentWinner = 0x0000000000000000000000000000000000000000;
 
@@ -186,7 +247,9 @@ contract WobMiningAndSmelting is Ownable(msg.sender), ReentrancyGuard {
     }
 
     function readYieldConfiguration() external view returns (uint8) {
-        return IBlast(0x4300000000000000000000000000000000000002).readYieldConfiguration(address(this));
+        return
+            IBlast(0x4300000000000000000000000000000000000002)
+                .readYieldConfiguration(address(this));
     }
 
     // Setters for parameters (onlyOwner)
@@ -211,6 +274,17 @@ contract WobMiningAndSmelting is Ownable(msg.sender), ReentrancyGuard {
     }
 
     function updateWobTokenAddress(address newWobToken) external onlyOwner {
-         wobToken = IERC20(newWobToken);
+        wobToken = IERC20(newWobToken);
+    }
+
+    function setSmeltingRequirement(uint256 newOreRequirement)
+        external
+        onlyOwner
+    {
+        smeltingOreRequirement = newOreRequirement;
+    }
+
+    function setSmeltingRewards(uint256 newRewards) external onlyOwner {
+        smeltingWobRewards = newRewards;
     }
 }
