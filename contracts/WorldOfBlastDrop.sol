4ec3d10d420eb3971ef4d833e6c86fc8e77ea885
David S <dxs@hotmail.ca>
2024-12-11 00:14:21 -0500

    fix: original drop removed unnessicary stuff, and fixed yield claim

--- contracts/WorldOfBlastDrop.sol
+++ contracts/WorldOfBlastDrop.sol
@@ -1,138 +1,10 @@
 // SPDX-License-Identifier: MIT
-pragma solidity 0.8.27;
+pragma solidity 0.8.28;
 import "@openzeppelin/contracts/token/ERC721/IERC721.sol";
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
-
-enum YieldMode {
-    AUTOMATIC,
-    VOID,
-    CLAIMABLE
-}
-enum GasMode {
-    VOID,
-    CLAIMABLE
-}
-
-interface IERC20Rebasing {
-    function configure(YieldMode _mode) external returns (uint256);
-
-    function claim(address recipient, uint256 amount)
-        external
-        returns (uint256);
-
-    function getClaimableAmount(address account)
-        external
-        view
-        returns (uint256);
-}
-
-interface IBlastPoints {
-    function configurePointsOperator(address operator) external;
-
-    function configurePointsOperatorOnBehalf(
-        address contractAddress,
-        address operator
-    ) external;
-}
-
-interface IBlast {
-    // configure
-    function configureContract(
-        address contractAddress,
-        YieldMode _yield,
-        GasMode gasMode,
-        address governor
-    ) external;
-
-    function configure(
-        YieldMode _yield,
-        GasMode gasMode,
-        address governor
-    ) external;
-
-    // base configuration options
-    function configureClaimableYield() external;
-
-    function configureClaimableYieldOnBehalf(address contractAddress) external;
-
-    function configureAutomaticYield() external;
-
-    function configureAutomaticYieldOnBehalf(address contractAddress) external;
-
-    function configureVoidYield() external;
-
-    function configureVoidYieldOnBehalf(address contractAddress) external;
-
-    function configureClaimableGas() external;
-
-    function configureClaimableGasOnBehalf(address contractAddress) external;
-
-    function configureVoidGas() external;
-
-    function configureVoidGasOnBehalf(address contractAddress) external;
-
-    function configureGovernor(address _governor) external;
-
-    function configureGovernorOnBehalf(
-        address _newGovernor,
-        address contractAddress
-    ) external;
-
-    // claim yield
-    function claimYield(
-        address contractAddress,
-        address recipientOfYield,
-        uint256 amount
-    ) external returns (uint256);
-
-    function claimAllYield(address contractAddress, address recipientOfYield)
-        external
-        returns (uint256);
-
-    // claim gas
-    function claimAllGas(address contractAddress, address recipientOfGas)
-        external
-        returns (uint256);
-
-    function claimGasAtMinClaimRate(
-        address contractAddress,
-        address recipientOfGas,
-        uint256 minClaimRateBips
-    ) external returns (uint256);
-
-    function claimMaxGas(address contractAddress, address recipientOfGas)
-        external
-        returns (uint256);
-
-    function claimGas(
-        address contractAddress,
-        address recipientOfGas,
-        uint256 gasToClaim,
-        uint256 gasSecondsToConsume
-    ) external returns (uint256);
-
-    // read functions
-    function readClaimableYield(address contractAddress)
-        external
-        view
-        returns (uint256);
-
-    function readYieldConfiguration(address contractAddress)
-        external
-        view
-        returns (uint8);
-
-    function readGasParams(address contractAddress)
-        external
-        view
-        returns (
-            uint256 etherSeconds,
-            uint256 etherBalance,
-            uint256 lastUpdated,
-            GasMode
-        );
-}
+import "./interfaces/IBlast.sol";
+import "./interfaces/IBlastPoints.sol";
 
 interface WorldOfBlastNft {
     function restoreNFT(uint256 tokenId) external;
@@ -152,14 +24,8 @@ contract WorldOfBlastDrop is Ownable {
         IBlast(0x4300000000000000000000000000000000000002);
 
     address public USDB_ADDRESS = 0x4300000000000000000000000000000000000003;
-    address public WETH_ADDDRES = 0x4300000000000000000000000000000000000004;
+    address public WETH_ADDRESS = 0x4300000000000000000000000000000000000004;
     address public CONTRACT_NFT = 0xFB7acDaE5B59e9C3337203830aEC1563316679E6;
-
-    IERC20Rebasing public constant USDB =
-        IERC20Rebasing(0x4300000000000000000000000000000000000003);
-
-    IERC20Rebasing public constant WETH =
-        IERC20Rebasing(0x4300000000000000000000000000000000000004);
 
     uint256 private RATE = 54697070639;
 
@@ -248,9 +114,6 @@ contract WorldOfBlastDrop is Ownable {
         BLAST.configureClaimableYield();
         BLAST.configureClaimableGas();
 
-        USDB.configure(YieldMode.CLAIMABLE);
-        WETH.configure(YieldMode.CLAIMABLE);
-
         for (uint256 i = 0; i < weights.length; i++) {
             totalWeight += weights[i];
         }
@@ -315,7 +178,7 @@ contract WorldOfBlastDrop is Ownable {
         onlyAuthorizedContract
         returns (uint256)
     {
-        IERC20 currentToken = IERC20(WETH_ADDDRES);
+        IERC20 currentToken = IERC20(WETH_ADDRESS);
 
         uint256 currentAmount = currentToken.balanceOf(address(this));
 
@@ -389,63 +252,9 @@ contract WorldOfBlastDrop is Ownable {
     }
 
     // Blast functions
-    function claimAllGas() external onlyOwner {
-        BLAST.claimAllGas(address(this), msg.sender);
-    }
-
-    function claimYieldTokens(address _recipient, uint256 _amount)
-        external
-        onlyOwner
-        returns (uint256, uint256)
-    {
-        return (
-            USDB.claim(_recipient, _amount),
-            WETH.claim(_recipient, _amount)
-        );
-    }
-
-    function getClaimableAmount(address _account)
-        external
-        view
-        returns (uint256, uint256)
-    {
-        return (
-            USDB.getClaimableAmount(_account),
-            WETH.getClaimableAmount(_account)
-        );
-    }
-
     function updatePointsOperator(address _newOperator) external onlyOwner {
         IBlastPoints(0x2536FE9ab3F511540F2f9e2eC2A805005C3Dd800)
             .configurePointsOperatorOnBehalf(address(this), _newOperator);
-    }
-
-    function configureClaimableYieldOnBehalf() external onlyOwner {
-        BLAST.configureClaimableYieldOnBehalf(address(this));
-    }
-
-    function configureAutomaticYieldOnBehalf() external onlyOwner {
-        BLAST.configureAutomaticYieldOnBehalf(address(this));
-    }
-
-    function configureVoidYield() external onlyOwner {
-        BLAST.configureVoidYield();
-    }
-
-    function configureVoidYieldOnBehalf() external onlyOwner {
-        BLAST.configureVoidYieldOnBehalf(address(this));
-    }
-
-    function configureClaimableGasOnBehalf() external onlyOwner {
-        BLAST.configureClaimableGasOnBehalf(address(this));
-    }
-
-    function configureVoidGas() external onlyOwner {
-        BLAST.configureVoidGas();
-    }
-
-    function configureVoidGasOnBehalf() external onlyOwner {
-        BLAST.configureVoidGasOnBehalf(address(this));
     }
 
     function claimYield(address recipient, uint256 amount) external onlyOwner {
@@ -469,19 +278,6 @@ contract WorldOfBlastDrop is Ownable {
 
     function claimMaxGas(address recipientOfGas) external onlyOwner {
         BLAST.claimMaxGas(address(this), recipientOfGas);
-    }
-
-    function claimGas(
-        address recipientOfGas,
-        uint256 gasToClaim,
-        uint256 gasSecondsToConsume
-    ) external onlyOwner {
-        BLAST.claimGas(
-            address(this),
-            recipientOfGas,
-            gasToClaim,
-            gasSecondsToConsume
-        );
     }
 
     function readClaimableYield() external view returns (uint256) {
